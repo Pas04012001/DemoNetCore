46add06be5dd22a29d5a3f3aa1ac69b79ee82ece
unknown <ehero2001@gmail.com>
2023-03-06 00:10:56 -0800

    btl

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers;
+
+public class EmployeeController : Controller
+{
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController(ILogger<EmployeeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
